482fb496e216b150e59956587f79166e2bb96634
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2017-07-13 18:00:25 -0500

    Add createData method in MainActivity

--- app/src/main/java/cibertec/pe/recetapp/MainActivity.java
+++ app/src/main/java/cibertec/pe/recetapp/MainActivity.java
@@ -2,12 +2,41 @@ package cibertec.pe.recetapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cibertec.pe.recetapp.Entities.Receta;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerViewRecetas;
+    List<Receta> recetaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewRecetas = (RecyclerView) findViewById(R.id.recyclerRecetas);
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerViewRecetas.setLayoutManager(linearLayoutManager);
+
+        crearData();
+    }
+
+    public void crearData(){
+        recetaList = new ArrayList<>();
+        recetaList.add(new Receta("1","Papa rellena",4,"Una comida peruana","Cocer la papa","image",0));
+        recetaList.add(new Receta("2","Palta rellena",2,"Una comida peruana","Cocer la palta","image",0));
+        recetaList.add(new Receta("3","Yuca rellena",3,"Una comida peruana","Cocer la yuca","image",1));
+        recetaList.add(new Receta("4","Aji de gallina",3,"Una comida peruana","Cocer la gallina","image",1));
+        recetaList.add(new Receta("5","Rocoto relleno",3,"Una comida peruana","Cocer el rocoto","image",1));
+        recetaList.add(new Receta("6","Carapulcra",3,"Una comida peruana","Cocinar el arroz","image",0));
+        recetaList.add(new Receta("7","Arroz con pato",3,"Una comida peruana","Cocer el pato","image",1));
     }
 }
