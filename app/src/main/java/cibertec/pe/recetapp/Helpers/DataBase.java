a13118d345d0b326bb60493a61b2b2061764ccdf
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2017-07-18 23:07:47 -0500

    Add methods in Database Helper

--- app/src/main/java/cibertec/pe/recetapp/Helpers/DataBase.java
+++ app/src/main/java/cibertec/pe/recetapp/Helpers/DataBase.java
@@ -1,8 +1,15 @@
 package cibertec.pe.recetapp.Helpers;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.DatabaseUtils;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.List;
+
+import cibertec.pe.recetapp.Entities.Receta;
 
 /**
  * Created by Julio on 18/07/2017.
@@ -26,5 +33,30 @@ public class DataBase {
 
     public void closeDatabase(){
         sqLiteOpenHelper.close();
+    }
+
+    public void insertReceta(Receta receta){
+        ContentValues contentValues = receta.toValues();
+        sqLiteDatabase.insert(SQLConstants.TABLE_RECETAS,null, contentValues);
+    }
+
+    //Me retorna el numero de registros que tiene mi tabla
+    public Long getItemCounts(){
+        return DatabaseUtils.queryNumEntries(sqLiteDatabase,SQLConstants.TABLE_RECETAS);
+    }
+
+    //Para insertar varias recetas a la vez
+    public void insertRecetas(List<Receta> recetas){
+        long items = getItemCounts();
+        if(items == 0)//Solo vamos a insertar los datos, si esta vacia la tabla
+        {
+            for (Receta receta: recetas) {
+                try{
+                    insertReceta(receta);
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
