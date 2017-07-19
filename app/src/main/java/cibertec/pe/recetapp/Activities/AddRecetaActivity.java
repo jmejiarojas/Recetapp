823c842b022c41cc87a02d35de741278b7a857d6
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2017-07-18 23:23:56 -0500

    Create AddReceta Activity

---
+++ app/src/main/java/cibertec/pe/recetapp/Activities/AddRecetaActivity.java
@@ -0,0 +1,15 @@
+package cibertec.pe.recetapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cibertec.pe.recetapp.R;
+
+public class AddRecetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_receta);
+    }
+}
