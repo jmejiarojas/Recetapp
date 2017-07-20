6c2a6f8147997cd74eef9d6abfa38f8b19c5d551
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2017-07-20 17:18:00 -0500

    Add feature to filter favorite recetas since menu_item fav

--- app/src/main/java/cibertec/pe/recetapp/Activities/MainActivity.java
+++ app/src/main/java/cibertec/pe/recetapp/Activities/MainActivity.java
@@ -126,7 +126,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.itemFavoritos:
-                Toast.makeText(this, "Favs", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "Favs", Toast.LENGTH_SHORT).show();
+                recetasAdapter = new RecetasAdapter(this, data.getFavoritos());
+                recyclerViewRecetas.setAdapter(recetasAdapter);
                 return true;
             case R.id.itemPersonas:
                 Toast.makeText(this, "Personas", Toast.LENGTH_SHORT).show();
